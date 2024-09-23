ec6569bd4609f2bdc3c96e69e624295eb13aa52a
Hans Askov <hansaskov1999@gmail.com>
2024-09-23 14:48:49 +0200

    first commit

---
+++ src/components/Query.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { getAll } from "../queries/select";
+
+    let results: any[] = [];
+    let input: number;
+
+    async function setRes() {
+        results = await getAll(input);
+    }
+</script>
+
+<form on:submit|preventDefault={setRes}>
+    <label>
+        Select amount
+        <fieldset role="group">
+            <input
+                name="select_amount"
+                placeholder="Select Amount"
+                type="number"
+                bind:value={input}
+            />
+            <input type="submit" value="Submit" />
+        </fieldset>
+    </label>
+</form>
+
+{#each results as result}
+    {result.Name}
+{/each}
+
