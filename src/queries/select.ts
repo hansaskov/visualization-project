bc8459cb50bce50a11f2e717d50c2f4bfb099dbd
Hans Askov <hans@askov.dk>
2024-09-23 21:58:43 +0200

    Update query to order by name

--- src/queries/select.ts
+++ src/queries/select.ts
@@ -2,5 +2,10 @@ import { c } from "./duckdb";
 
 
 export async function getAll(n: number) {
-    return await c.query(`SELECT * FROM data LIMIT ${n}`)
+    return await c.query(`
+        SELECT * 
+        FROM data 
+        ORDER BY Name
+        LIMIT ${n}
+    `);
 }
