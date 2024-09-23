8ad6978d22719df89d351a05d838249d38962212
Hans Askov <hans@askov.dk>
2024-09-23 21:23:55 +0200

    Simplify queries

--- src/queries/select.ts
+++ src/queries/select.ts
@@ -1,10 +1,6 @@
 import { c } from "./duckdb";
 
-const prepared = await c.prepare(`SELECT * FROM data LIMIT ?`);
 
 export async function getAll(n: number) {
-    const result = await prepared.query(n)
-    const json = result.toArray().map((row) => row.toJSON());
-
-    return json
+    return await c.query(`SELECT Name FROM data LIMIT ${n}`)
 }
