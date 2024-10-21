533bf8e9a0804d2a32550c262f3ee16af4209cec
Hans Askov <hansaskov1999@gmail.com>
2024-10-21 09:16:07 +0200

    Created functions for top level await

--- src/queries/duckdb.ts
+++ src/queries/duckdb.ts
@@ -3,17 +3,39 @@ import * as duckdb from '@duckdb/duckdb-wasm';
 
 const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 
-// Select a bundle based on browser checks
-const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+async function createDuckDB() {
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
 
-const worker_url = URL.createObjectURL(
-  new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
-);
+  const worker_url = URL.createObjectURL(
+    new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
+  );
 
-// Instantiate the asynchronus version of DuckDB-Wasm
-const worker = new Worker(worker_url);
-const logger = new duckdb.ConsoleLogger();
-export const db = new duckdb.AsyncDuckDB(logger, worker);
-await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-URL.revokeObjectURL(worker_url);
+  // Instantiate the asynchronus version of DuckDB-Wasm
+  const worker = new Worker(worker_url);
+  const logger = new duckdb.ConsoleLogger();
+  const db = new duckdb.AsyncDuckDB(logger, worker);
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+  URL.revokeObjectURL(worker_url);
+
+  return db;
+}
+
+async function createDuckDBConnection(db: duckdb.AsyncDuckDB) {
+  const serverUrl = import.meta.env.DEV ? "http://localhost:4321" : "https://visualization-project.pages.dev";
+  const c = await db.connect();
+  await c.insertCSVFromPath(`${serverUrl}/data.csv`, {
+      name: 'data',
+      detect: true,
+      header: true,
+  });
+
+  return c;
+  
+}
+
+export const db = await createDuckDB();
+export const c = await createDuckDBConnection(db);
+
+
 
--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,12 +1,4 @@
-import { db } from "./duckdb";
-
-const serverUrl = import.meta.env.DEV ? "http://localhost:4321" : "https://visualization-project.pages.dev";
-const c = await db.connect();
-await c.insertCSVFromPath(`${serverUrl}/data.csv`, {
-    name: 'data',
-    detect: true,
-    header: true,
-});
+import { db, c } from "./duckdb";
 
 export async function executeQuery(queryString: string): Promise<Record<string, any>[] | Error> {
     try {
